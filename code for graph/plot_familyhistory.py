1ca0f4befe21e3cfa5548863acf7e1afed948c13
YuqiuHuang <yqhuang2004@gmail.com>
2025-12-04 15:47:50 +0100

    code

---
+++ code for graph/plot_familyhistory.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../data/ObesityDataSet_raw_and_data_sinthetic.csv")
+df = df.rename(columns={'NObeyesdad': 'obesity level'})
+crosstab_table = pd.crosstab(df['family_history_with_overweight'],df['obesity level'])
+crosstab_table.index = ['No family history', 'Has family history']
+crosstab_table.columns = ['less weight', 'Normal weight', 'Overweight I', 'Overweight II','Obesity I', 'Obesity II', 'Obesity III']
+
+print(crosstab_table)
+ax = crosstab_table.T.plot(kind='bar')
+ax.set_xlabel("Obesity level")
+ax.set_ylabel("Number of participants")
+ax.set_title("Obesity vs. family history of overweight")
+plt.xticks(rotation=20)
+plt.tight_layout()
+plt.savefig("figures/family_history_vs_obesity.png", dpi=300)111
+plt.show()
