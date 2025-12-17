049858f1c1f5a28b4dc7a4c7bf12111605005eca
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-12-17 17:26:57 +0100

    fix deps

--- regression log.py
+++ regression log.py
@@ -1,17 +1,22 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
-df = pd.read_csv("data/ObesityDataSet_raw_and_data_sinthetic.csv")
-df['obesity_binary']= 0
-df.loc[df['NObeyesdad']!='Normal_Weight','obesity_binary']= 1
 
-df['family_history_with_overweight']= df['family_history_with_overweight'].map({'yes': 1,'no': 0})
-df['SMOKE']=df['SMOKE'].map({'yes':1,'no':0})
-df['FAVC']=df['FAVC'].map({'yes': 1, 'no': 0})
-df['CAEC'] = df['CAEC'].map({'no': 0,'Sometimes': 1,'Frequently': 2,'Always': 3})
+df = pd.read_csv("data/ObesityDataSet_raw_and_data_sinthetic.csv")
+df["obesity_binary"] = 0
+df.loc[df["NObeyesdad"] != "Normal_Weight", "obesity_binary"] = 1
+
+df["family_history_with_overweight"] = df["family_history_with_overweight"].map(
+    {"yes": 1, "no": 0},
+)
+df["SMOKE"] = df["SMOKE"].map({"yes": 1, "no": 0})
+df["FAVC"] = df["FAVC"].map({"yes": 1, "no": 0})
+df["CAEC"] = df["CAEC"].map({"no": 0, "Sometimes": 1, "Frequently": 2, "Always": 3})
 df = df.dropna()
-X = df[['family_history_with_overweight', 'SMOKE', 'FAVC', 'CAEC']]
+X = df[["family_history_with_overweight", "SMOKE", "FAVC", "CAEC"]]
 X = sm.add_constant(X)
-y = df['obesity_binary']
+y = df["obesity_binary"]
 model = sm.Logit(y, X).fit()
+plt.show()
 
 print(model.summary())
