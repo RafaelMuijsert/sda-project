78311ea5eb66889501478508d7f194495c2c8997
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-12-18 15:04:11 +0100

    clean up ordinal logistic regression

--- src/ordinal_logistic_regression.py
+++ src/ordinal_logistic_regression.py
@@ -1,20 +1,41 @@
+"""Ordinary logistic regression.
+
+Copyright (C) 2025.
+"""
+
 import pandas as pd
-from statsmodels.miscmodels.ordinal_model import OrderedModel
+from statsmodels.miscmodels.ordinal_model import (
+    OrderedModel,
+    OrderedResults,
+)
 
 DATASET_PATH = "data/obesity_cleaned_final.csv"
 
 
-def read_data(filename):
-    df = pd.read_csv(filename, sep=";")
-    return df
+def read_data(filename: str) -> pd.DataFrame:
+    """Read the dataset.
+
+    Returns:
+        pd.DataFrame: the dataset as a pandas dataframe.
+
+    """
+    return pd.read_csv(filename, sep=";")
 
 
-def prepare_for_ordinal_regression(df):
-    # Target (ordinal)
-    y = df["Obesity_Level"]
+def prepare_for_ordinal_regression(
+    df: pd.DataFrame,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Prepare the dataset for ordinal logistic regression.
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: the original target and the predictors
+
+    """
+    # Ordinal target
+    y: pd.DataFrame = df["Obesity_Level"]
 
     # Predictors
-    X = df[
+    x: pd.DataFrame = df[
         [
             "Gender",
             "Age",
@@ -23,28 +44,37 @@ def prepare_for_ordinal_regression(df):
             "MTRANS_Bike",
             "MTRANS_Motorbike",
             "MTRANS_Public_Transportation",
-            # MTRANS_Walking is DROPPED (baseline) because including it made the sum of MTRANS=1
+            # MTRANS_Walking is DROPPED (baseline) because it made the sum of MTRANS=1
         ]
     ]
 
-    return X, y
+    return x, y
 
 
-def run_ordinal_logistic_regression(X, y):
-    model = OrderedModel(y, X, distr="logit")
+def run_ordinal_logistic_regression(
+    x: pd.DataFrame,
+    y: pd.DataFrame,
+) -> OrderedResults:
+    """Run ordinal logistich regression on the given target and predictors.
 
-    result = model.fit(method="bfgs")
-    return result
+    Returns:
+        OrderedResultsWrapper: the results of the ordinary logistic regression
+
+    """
+    model = OrderedModel(y, x, distr="logit")
+
+    return model.fit(method="bfgs")
 
 
-def main():
+def main() -> None:
+    """Read and prepare the dataset and perform ordinal logistic regression."""
     df = read_data(DATASET_PATH)
 
-    X, y = prepare_for_ordinal_regression(df)
+    x, y = prepare_for_ordinal_regression(df)
 
-    result = run_ordinal_logistic_regression(X, y)
+    result = run_ordinal_logistic_regression(x, y)
 
-    print(result.summary())
+    print(result.summary())  # noqa: T201
 
 
 if __name__ == "__main__":
